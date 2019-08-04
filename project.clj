04a0bc7960483a4bc34b381e9a86018b864c66cc
Toby Clemson <tobyclemson@gmail.com>
2019-08-04 19:35:40 +0100

    Version 0.0.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/antsy "0.0.8"
+(defproject io.logicblocks/antsy "0.0.9-SNAPSHOT"
   :description "Simple ANSI escape code library for Clojure."
   :url "https://github.com/logicblocks/antsy"
 
