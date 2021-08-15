4f33f7e06a0628a9491ed8ca5853a9b77f480351
Circle CI <circleci@infrablocks.io>
2021-08-15 11:14:42 +0000

    Pre-release version 0.0.16-RC2 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/antsy "0.0.16-RC1"
+(defproject io.logicblocks/antsy "0.0.16-RC2"
   :description "Simple ANSI escape code library for Clojure."
   :url "https://github.com/logicblocks/antsy"
 
